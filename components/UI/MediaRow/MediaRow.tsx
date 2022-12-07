a94538aef2f354bbd5f9b261e0fd692283cf161b
Aaron <aarongrossman.dev@gmail.com>
2022-12-07 10:51:44 -0500

    Added dynamic api to movies/series/genres section

--- components/UI/MediaRow/MediaRow.tsx
+++ components/UI/MediaRow/MediaRow.tsx
@@ -1,29 +1,74 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import {shuffleArray} from '../../Utilities';
 
+const MediaRow = (props: any) => {
+  const [loadingData, setLoadingData] = useState(true);
+  const [movies, setMoviesData] = useState([]);
+  
+//   /discover/movie?with_genres=28&primary_release_year=2022
+  useEffect(() => {
+    axios
+      .get(`https://api.themoviedb.org/3/${props.endpoint}&api_key=4c84c6329ec1b7a3dc0a99038a8f621a&language=en-US`)
+      .then(function (response) {
+        setMoviesData(shuffleArray(response.data.results))
+        setLoadingData(false);
+        // handle success
+        console.log('Success response for ' + props.title);
+        console.log(response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log('error response for ' + props.title);
+        console.log(error);
+      })
+  }, []);
 
-const JustAdded = (props: any) => {
-    const loopComp = (comp: any, digit: any) => {
-        let thumbnails = [];
-        for(let index = 1; index <= digit; index++){
-            thumbnails.push(comp)
-        }
-        return thumbnails;
+  const loopComp = (comp: any, digit: any) => {
+    let thumbnails = [];
+    for (let index = 1; index <= digit; index++) {
+      thumbnails.push(comp);
     }
-    return(
-        <div className="just-added">
-            <h3 className="just-added__title">For You</h3>
-            <div className="just-added__thumbnails">
-                
-                {loopComp(
-                    (<div className="just-added__thumbnail">
-                    <img src="https://i5.walmartimages.com/asr/86bae80c-9bc6-48b2-bc49-da9112d15f8e.09e5625dc667619a33e1d359b8644472.jpeg"/>
-                    <div className="just-added__top-layer">
-                        <i className="fas fa-play"/>
-                    </div>
-                </div>), 10)}
-               
-            </div>
-        </div>
-    )
-}
+    return thumbnails;
+  };
+  const showThumbnails = () => {
+    
+    return loadingData
+      ? loopComp(<Skeleton />, 10)
+      : movies.map((movie) => {
+        return <Thumbnail movieData={movie} />
+      });
+  };
+  return (
+    <div className={`media-row ${props.type}`}>
+      <h3 className="media-row__title">{props.title}</h3>
+      <div className="media-row__thumbnails">
+        {showThumbnails()}
 
-export default JustAdded;
+        {/* {loopComp(
+                    (<Thumbnail />), 10)} */}
+      </div>
+    </div>
+  );
+};
+
+const Thumbnail = (props: any) => {
+  return (
+    <div className="media-row__thumbnail">
+      <img src={`https://image.tmdb.org/t/p/original${props.movieData.poster_path}`}/>
+      <div className="media-row__top-layer">
+        <i className="fas fa-play" />
+      </div>
+    </div>
+  );
+};
+
+const Skeleton = () => {
+  return (
+    <div className="media-row__thumbnail-skeleton">
+      <div className="media-row__thumbnail-skeleton-img"></div>
+    </div>
+  );
+};
+
+export default MediaRow;
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,8 @@ import { useStateContext } from "../components/HBOProvider";
 import { useRouter } from "next/router";
 import MainLayout from "../components/Layouts/MainLayout";
 import FeaturedMedia from "../components/UI/FeaturedMedia/FeaturedMedia";
-import ForYouList from "../components/UI/ForYouList/ForYouList";
-import JustAdded from "../components/UI/JustAdded/JustAdded";
-import PosterView from "../components/UI/PosterView/PosterView";
 import MediaRow from "../components/UI/MediaRow/MediaRow";
-import AuthCheck from '../components/AuthCheck';
+import AuthCheck from "../components/AuthCheck";
 
 export default function Home() {
   const globalState = useStateContext();
@@ -17,10 +14,44 @@ export default function Home() {
   return AuthCheck(
     <MainLayout>
       <FeaturedMedia />
-      <MediaRow />
-      <ForYouList />
-      <JustAdded />
-      <PosterView />
+      <MediaRow
+        title="Movies"
+        type="large-h"
+        endpoint="discover/movie?sort_by=popularity.desc&primary_release_year=2021"
+      />
+      <MediaRow
+        title="Series"
+        type="small-h"
+        endpoint="discover/tv?sort_by=popularity.desc&primary_release_year=2022"
+      />
+      <MediaRow
+        title="Action"
+        type="large-v"
+        endpoint="discover/movie?with_genres=28&primary_release_year=2022"
+      />
+      <MediaRow
+        title="Horror"
+        type="small-v"
+        endpoint="discover/movie?with_genres=27&primary_release_year=2022"
+      />
+      <MediaRow
+        title="Animations"
+        type="large-h"
+        endpoint="discover/movie?with_genres=16&primary_release_year=2022"
+      />
+      <MediaRow
+        title="Sci-fi"
+        type="small-v"
+        endpoint="discover/movie?with_genres=878&primary_release_year=2022"
+      />
     </MainLayout>
   );
 }
+
+//API TO LOAD ALL DATA----setup through backend
+//api.hbomax.com/api/homepage JSON with all of the data for that page
+
+// API to load individuals
+//api.hbomax.com/api/home/movies
+//api.hbomax.com/api/home/series
+//api.hbomax.com/api/home/action
