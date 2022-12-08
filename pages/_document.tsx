70bb40e32ee882dbe6499bb1f8b5796f6099a046
Aaron <aarongrossman.dev@gmail.com>
2022-12-07 22:08:38 -0500

    added fontawesome to local files

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -26,7 +26,7 @@ class MyDocument extends Document {
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""/>
         <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&family=Noto+Sans:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"/>
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossOrigin="anonymous"/>
+        <link rel="stylesheet" href="/fontawesome/css/all.min.css"/>
         </Head>
         <body>
           <Main />
